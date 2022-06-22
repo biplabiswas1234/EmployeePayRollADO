fd02c658d32be671723cfae946d0b0db74a91ad9
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-22 12:38:00 +0530

    [Biplab] Add: Inserting data to database

---
+++ EmployeePayrollADO/EmpTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmpTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fa0021a-e406-4899-b219-bfed729f3ef1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- EmployeePayrollADO/EmployeePayrollADO/Employee.cs
+++ EmployeePayrollADO/EmployeePayrollADO/Employee.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeePayrollADO
 {
@@ -20,5 +18,6 @@ namespace EmployeePayrollADO
         public double TaxablePay { get; set; }
         public double IncomeTax { get; set; }
         public double NetPay { get; set; }
+
     }
 }
