e8e0acd0a06b9695b5cca0c977fb0bbed14193d8
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-22 12:57:57 +0530

    [Biplab] Add: Remove data from database

--- EmployeePayrollADO/EmpTest/UnitTest1.cs
+++ EmployeePayrollADO/EmpTest/UnitTest1.cs
@@ -5,62 +5,71 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EmpTest
 {
-    public class Tests
+    Employee employee;
+    EmployeeDetail employeeDetail;
+    [SetUp]
+    public void Setup()
     {
-        Employee employee;
-        EmployeeDetail employeeDetail;
-        [SetUp]
-        public void Setup()
-        {
-            employee = new Employee();
-            employeeDetail = new EmployeeDetail();
-        }
-        //<summary>
-        //TC 2 : Insert Details
-        //</summary>
-        [Test]
-        public void AddingEmployeeDetails()
-        {
-            int expected = 1;
-            employee.Name = "Hails";
-            employee.StartDate = Convert.ToDateTime("2022-10-03");
-            employee.Gender = "M";
-            employee.PhoneNumber = 9856742311;
-            employee.Address = "Mulund";
-            employee.Department = "Civil";
-            employee.BasicPay = 33300;
-            employee.Deduction = 4400;
-            employee.TaxablePay = 1000;
-            employee.IncomeTax = 1200;
-            employee.NetPay = 25000;
-            var actual = employeeDetail.InsertEmployeeData(employee);
-            Assert.AreEqual(expected, actual);
-        }
-        //<summary>
-        //TC 3 : Update Details
-        //</summary>
-        [Test]
-        public void UpdatingEmployeeDetails()
-        {
-            employee.ID = 4;
-            employee.Name = "Athena";
-            employee.BasicPay = 56500;
-            var result = employeeDetail.UpdateSalary(employee);
-            var expected = result.BasicPay;
-            var actual = result.BasicPay;
-            Assert.AreEqual(expected, actual);
-        }
-        //<summary>
-        //TC 4 : Retrieve Data in DateRange
-        //</summary>
-        [Test]
-        public void Retrieving_Data_In_DateRange()
-        {
-            var fromDate = Convert.ToDateTime("2022-01-01");
-            var toDate = Convert.ToDateTime("2022-10-01");
-            var result = employeeDetail.EmployeeData_InDataRange(fromDate, toDate);
-            var expected = result.Count;
-            Assert.AreEqual(expected, result.Count);
-        }
+        employee = new Employee();
+        employeeDetail = new EmployeeDetail();
     }
+    //<summary>
+    //TC 2 : Insert Details
+    //</summary>
+    [Test]
+    public void AddingEmployeeDetails()
+    {
+        int expected = 1;
+        employee.Name = "Hails";
+        employee.StartDate = Convert.ToDateTime("2022-10-03");
+        employee.Gender = "M";
+        employee.PhoneNumber = 9856742311;
+        employee.Address = "Mulund";
+        employee.Department = "Civil";
+        employee.BasicPay = 33300;
+        employee.Deduction = 4400;
+        employee.TaxablePay = 1000;
+        employee.IncomeTax = 1200;
+        employee.NetPay = 25000;
+        var actual = employeeDetail.InsertEmployeeData(employee);
+        Assert.AreEqual(expected, actual);
+    }
+    //<summary>
+    //TC 3 : Update Details
+    //</summary>
+    [Test]
+    public void UpdatingEmployeeDetails()
+    {
+        employee.ID = 4;
+        employee.Name = "Athena";
+        employee.BasicPay = 56500;
+        var result = employeeDetail.UpdateSalary(employee);
+        var expected = result.BasicPay;
+        var actual = result.BasicPay;
+        Assert.AreEqual(expected, actual);
+    }
+    //<summary>
+    //TC 4 : Retrieve Data in DateRange
+    //</summary>
+    [Test]
+    public void Retrieving_Data_In_DateRange()
+    {
+        var fromDate = Convert.ToDateTime("2022-01-01");
+        var toDate = Convert.ToDateTime("2022-10-01");
+        var result = employeeDetail.EmployeeData_InDataRange(fromDate, toDate);
+        var expected = result.Count;
+        Assert.AreEqual(expected, result.Count);
+    }
+    //<summary>
+    //TC 5 : Remove Details
+    //</summary>
+    [Test]
+    public void RemovingEmployeeDetails()
+    {
+        bool expected = true;
+        employee.ID = 5;
+        bool result = employeeDetail.RemoveDetails(employee);
+        Assert.AreEqual(expected, result);
+    }
+}
 }
